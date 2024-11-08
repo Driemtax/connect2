dfeeecb73351ab651f968b1d6a0bd6e51d49a89b
Lukas Heberling <62028575+N3moAhead@users.noreply.github.com>
2024-11-08 19:23:21 +0100

    feat(home_view): Loading contacts from contact_service

--- lib/screens/home_view.dart
+++ lib/screens/home_view.dart
@@ -1,16 +1,73 @@
+import 'package:connect2/exceptions/exceptions.dart';
 import 'package:connect2/screens/person_card_view.dart';
+import 'package:connect2/services/contacts_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
 
 // HomeContent Widget: Die scrollbare Liste
-class HomeContent extends StatelessWidget {
+class HomeContent extends StatefulWidget {
   const HomeContent({super.key});
 
   @override
+  HomeContentState createState() => HomeContentState();
+}
+
+class HomeContentState extends State<HomeContent> {
+  List<Contact> contacts = [];
+  bool permissionDenied = false;
+
+  void loadContacts() async {
+    try {
+      final fetchedContacts = await getContacts();
+      setState(() {
+        contacts = fetchedContacts;
+      });
+    } on PermissionDeniedException {
+      setState(() {
+        permissionDenied = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadContacts();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final List<String> names = [
-      'Alice Biden', 'Bob Schulz', 'Charlie', 'David', 'Eve', 'Frank',
-      'Grace', 'Heidi', 'Ivan', 'Judy', 'Karl', 'Lars', 'Marta', 'Nicole'
-    ];
+    // TODO prettify the view
+    if (permissionDenied) {
+      return Scaffold(
+        body: Center(
+          child: TextButton(
+            style: const ButtonStyle(
+              foregroundColor: WidgetStatePropertyAll(Colors.blue)
+            ),
+            onPressed: () {
+              loadContacts();
+            },
+            child: const Text('Give permissions to laod contacts'), // TODO add i18
+          ), // TODO add i18
+        )
+      );
+    }
+
+    final List<String> names = contacts.map((e) {
+      if (e.displayName.isEmpty) {
+        return 'No Name'; // TODO add i18
+      }
+      return e.displayName;
+    }).toList();
+
+    // TODO prettify the message
+    if (contacts.isEmpty) {
+      return const Scaffold(
+        body:
+            Center(child: Text('No contacts have been found.')), // TODO add i18
+      );
+    }
 
     return Scaffold(
       floatingActionButton: FloatingActionButton(
@@ -27,7 +84,7 @@ class HomeContent extends StatelessWidget {
         itemCount: names.length,
         itemBuilder: (context, index) {
           return ListTile(
-            leading: const Icon(Icons.person),  // Icon links vom Namen
+            leading: const Icon(Icons.person),
             title: Text(names[index]),
           );
         },
