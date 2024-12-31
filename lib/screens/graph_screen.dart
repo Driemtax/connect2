8f05074910ffd848c766e1d09af584a533b94d04
jaleneu <ja.neuhaus@proton.me>
2024-12-31 12:46:39 +0100

    feat: add english i18 support for graph_screen

--- lib/screens/graph_screen.dart
+++ lib/screens/graph_screen.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:connect2/components/graph_view/graph_view_canvas.dart';
 import 'package:connect2/components/graph_view/node.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
 
 class GraphScreen extends StatefulWidget {
   const GraphScreen({super.key});
@@ -37,9 +38,9 @@ class GraphScreenState extends State<GraphScreen> {
   Widget build(BuildContext context) {
     // TODO develop pretty empty state
     if (nodes.isEmpty) {
-      return const Scaffold(
+      return Scaffold(
         body: Center(
-          child: Text("Loading...")
+          child: Text(FlutterI18n.translate(context, "graph_screen.loading"))
         )
       );
     }
